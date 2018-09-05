f35e76b928d64f0b596d161294ea84e2387abf40
Quentin <qburg@cutii.io>
2018-09-05 19:21:54 +0200

    setup project with tools

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/react'],
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
