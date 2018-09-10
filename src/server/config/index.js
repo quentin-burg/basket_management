af12d398e30ce57f531dc27bdf913431d411e8e6
Quentin <qburg@cutii.io>
2018-09-10 18:36:50 +0200

    setup server API and database

---
+++ src/server/config/index.js
@@ -0,0 +1,11 @@
+const config = {
+  username      : 'mjjrkcbo',
+  password      : 'sUgM1onNGR6Zt-oa83wLJp6P42O98Wv_',
+  database      : 'mjjrkcbo',
+  host          : 'horton.elephantsql.com',
+  port          : 5432,
+  dialect       : 'postgres',
+  seederStorage : 'sequelize',
+};
+
+module.exports = config;
