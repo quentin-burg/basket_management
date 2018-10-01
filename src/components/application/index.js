6a6ddab41f6ec3787e924d8fb550f69f51ec2f1e
Quentin <qburg@cutii.io>
2018-10-01 11:33:14 +0200

    setup userId on root component

--- src/components/application/index.js
+++ src/components/application/index.js
@@ -1,18 +1,23 @@
 import React from 'react';
-import { Router, Route, Switch } from 'react-router';
+import { Router, RouteProps, Switch } from 'react-router';
 import Home from 'components/home';
 import Order from 'components/order';
 import Bill from 'components/bill';
 import history from 'utils/history';
+import PropTypes from 'prop-types';
 
-const Application = () => (
+const Application = ({ userId }) => (
   <Router history={history}>
     <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/order" component={Order} />
-      <Route path="/bill" component={Bill} />
+      <RouteProps exact path="/" component={Home} userId={userId} />
+      <RouteProps path="/order" component={Order} userId={userId} />
+      <RouteProps path="/bill" component={Bill} userId={userId} />
     </Switch>
   </Router>
 );
+
+Application.propTypes = {
+  userId : PropTypes.string.isRequired,
+};
 
 export default Application;
