d75bc56bb34689722b97ba32e3baf4366f41de66
Quentin <qburg@cutii.io>
2018-10-01 13:34:20 +0200

    fix console error

--- src/components/application/index.js
+++ src/components/application/index.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import { Router, RouteProps, Switch } from 'react-router';
+import { Router, Switch, Route } from 'react-router';
 import Home from 'components/home';
 import Order from 'components/order';
 import Bill from 'components/bill';
 import history from 'utils/history';
 import PropTypes from 'prop-types';
 
-const Application = ({ userId }) => (
+const Application = ({ userId = '35' }) => (
   <Router history={history}>
     <Switch>
-      <RouteProps exact path="/" component={Home} userId={userId} />
-      <RouteProps path="/order" component={Order} userId={userId} />
-      <RouteProps path="/bill" component={Bill} userId={userId} />
+      <Route exact path="/" render={() => <Home userId={userId} />} />
+      <Route path="/order" render={() => <Order userId={userId} />} />
+      <Route path="/bill" render={() => <Bill userId={userId} />} />
     </Switch>
   </Router>
 );
 
 Application.propTypes = {
-  userId : PropTypes.string.isRequired,
+  userId : PropTypes.string,
 };
 
 export default Application;
